c3a19c91cda4da741267d673a934efa550941c82
kiritocode1 <kathawalearyan9@gmail.com>
2024-07-27 23:51:10 +0530

    AUTH CALLBACK ERROR

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,10 +7,14 @@ import { auth } from "@/auth";
 
 async function UserAvatar() {
     const session = await auth();
-    if (session?.user?.image) { 
-        return <Image src={session.user.image} alt="avatar" className="w-12 h-12 rounded-full" />
+
+    if (!session || !session.user || !session.user.image) {
+        return <div className="w-12 h-12 rounded-full bg-gray-200">No</div>
     }
-    return <div className="w-12 h-12 rounded-full bg-gray-200">No</div>
+
+        return <Image src={session.user.image} alt="avatar" className="w-12 h-12 rounded-full" />
+
+    
 }
 
 
