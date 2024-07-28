21c79dd387d633fb58bacc2ea6a0320cd96160d9
kiritocode1 <kathawalearyan9@gmail.com>
2024-07-28 20:54:38 +0530

    hello

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,38 +1,37 @@
+import { FC, Suspense } from "react";
+import Image from "next/image";
 
-import { FC, Suspense } from 'react'
-import Image from 'next/image'
-
-
-interface pageProps {
-  
-}
+interface pageProps {}
 import { auth } from "@/auth";
-import { KanbanBoard } from '@/components/KanbanBoard';
+import { KanbanBoard } from "@/components/KanbanBoard";
 
 async function UserAvatar() {
-    const session = await auth();
+	const session = await auth();
 
-    if (typeof session === null  ) { return <div className='w-full min-h-screen'>{JSON.stringify(session)}</div> }
-    return <div className='w-full min-h-screen'>
-            <Image src={session?.user?.image!} alt="user avatar" width={100} height={100} />
-        </div>
-    
-};
-
-
-
-
-
-const page: FC<pageProps> = ({}) => {
-    return (
+	if (typeof session === null) {
+		return <div className="w-full min-h-screen">{JSON.stringify(session)}</div>;
+	}
+	return (
 		<div className="w-full min-h-screen">
-			<UserAvatar />
-
-			<Suspense fallback={<div>Loading...</div>}>
-				<KanbanBoard />
-			</Suspense>
+			<Image
+				src={session?.user?.image!}
+				alt="user avatar"
+				width={100}
+				height={100}
+			/>
 		</div>
 	);
 }
 
-export default page
+const page: FC<pageProps> = ({}) => {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<div className="w-full min-h-screen">
+				<UserAvatar />
+				<KanbanBoard />
+			</div>
+		</Suspense>
+	);
+};
+
+export default page;
